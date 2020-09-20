d516b6c8ea053cb50eedff01e853e84287db5d00
SamborVadym <samborvadym@gmail.com>
2020-09-20 12:46:39 +0300

    added all pages profile

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,17 @@
 $( document ).ready(function() {
   $('.open-popup-data').click(function () {
     $('.popup-data').addClass(' popup-open');
-  })
+  });
+
+  $('.profile-info-order-block-btn').click(function () {
+    $(this).parent().parent().parent().toggleClass('active');
+  });
+
+  $(".readOnly").rateYo({
+    rating: 3.2,
+    readOnly: true,
+    starWidth: "12px",
+    ratedFill: "#F0C963",
+  });
 });
 
