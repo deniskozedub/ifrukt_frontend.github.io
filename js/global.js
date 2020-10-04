bc49de73b8ee6404fb6fd4bc27889e3d2400ae20
CDenis <denclub2013@gmail.com>
2020-10-04 12:02:14 +0300

    added new pages

--- js/global.js
+++ js/global.js
@@ -9,7 +9,16 @@ $( document ).ready(function() {
     })
 
     $('.popup-close').click(function () {
-        $('.popup').removeClass(' popup-open');
+        $('.popup').removeClass('popup-open');
+        if ($('.main-responsive-slider-item.active').length) {
+            $('.main-responsive-slider-item.active').removeClass('active');
+        }
+    })
+    $('.popup-cart-item-close').click(function () {
+        $('.popup').removeClass('popup-open');
+        if ($('.main-responsive-slider-item.active').length) {
+            $('.main-responsive-slider-item.active').removeClass('active');
+        }
     })
 
     $('.popup-bg').click(function () {
@@ -24,4 +33,33 @@ $( document ).ready(function() {
             $(this).parent().children('.popup-input').attr('type',  'text');
         }
     });
+
+    $('.main-responsive-slider-item-button').click(function () {
+        $(this).parent().addClass('active');
+        $('.popup-cart').addClass('popup-open');
+        $('.popup-cart-item-image img').attr('src', $('.active .main-responsive-slider-item-image img').attr("src"));
+        $('.popup-cart-item-name').text($('.active .main-responsive-slider-item-name').text());
+        $(".popup-cart-item-price span").text($('.active .main-responsive-slider-item-price span').text());
+    })
+
+    $('.popup-cart-item-size-min').click(function() {
+        const num = +$('.product-num').text() - 1;
+        if (num === 0) {
+            $('.popup-cart-item-size-min').attr("disabled", 'disabled');
+        }
+        $('.product-num').text(num);
+        const price = parseInt($(".popup-cart-item-price span").text()) - parseInt($('.active .main-responsive-slider-item-price span').text());
+        $(".popup-cart-item-price span").text(price);
+    })
+    $('.popup-cart-item-size-plus').click(function() {
+        const num = +$('.product-num').text() + 1;
+        const price = parseInt($(".popup-cart-item-price span").text()) + parseInt($('.active .main-responsive-slider-item-price span').text());
+        $('.popup-cart-item-size-min').attr('disabled', false);
+        $('.product-num').text(num);
+        $(".popup-cart-item-price span").text(price);
+    });
+
+    $('.popup-cart-product').click(function(e) {
+        console.log(e.target);
+    })
 });
